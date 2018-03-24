e7d82e61c59b841e17ab439dbedc2bb5ded0b70d
AvvosMeyz <hmztkn@gmail.com>
2018-03-24 03:32:41 +0300

    text-shadow added

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
 /* hi everyiibodoo */ 
 
-document.getElementById('menuTrig').scrollIntoView({block: 'start', behavior: 'smooth'});
+//document.getElementById('menuTrig').scrollIntoView({block: 'start', behavior: 'smooth'});
 
